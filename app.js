2e8e99ba445d61b774dbe754d2da4f038f60a1bf
Delian Delchev <delian.delchev@gmail.com>
2014-06-09 19:36:06 +0000

    test

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ Collector(function(flow) {
                 if (n.protocolText == 'ethernet') {
                     try {
                         var pkt = pcap.decode.ethernet(n.header, 0);
+                        console.log(pkt);
                         if (pkt.ethertype==2048) return;
                         console.log('VLAN',pkt.vlan?pkt.vlan.id:'none','Packet',pkt.ip.protocol_name,pkt.ip.saddr,':',pkt.ip.tcp?pkt.ip.tcp.sport:pkt.ip.udp.sport,'->',pkt.ip.daddr,':',pkt.ip.tcp?pkt.ip.tcp.dport:pkt.ip.udp.dport)
-                    } catch(e) {}
+                    } catch(e) { console.log(e); }
                 }
             }
         });
