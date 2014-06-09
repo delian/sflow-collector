c642567a3e4aaa4893b21e38ea7f3a9140f08c39
Delian Delchev <delian.delchev@gmail.com>
2014-06-09 19:22:48 +0000

    test

--- app.js
+++ app.js
@@ -3,11 +3,18 @@
  */
 
 var Collector = require('node-sflow');
+var pcap = require('pcap');
 
 Collector(function(flow) {
     if (flow && flow.flow.records && flow.flow.records.length>0) {
         flow.flow.records.forEach(function(n) {
             console.log('process record',n);
+
+            if (n.type == 'raw') {
+                if (n.protocolText == 'ethernet') {
+                    console.log(decode.ethernet.pcap(n.header,0));
+                }
+            }
         });
     }
 }).listen(6344);
