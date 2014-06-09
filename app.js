db34c2c5e20b6102a50160060f6613cfd367bca3
Delian Delchev <delian.delchev@gmail.com>
2014-06-09 19:10:03 +0000

    test

--- app.js
+++ app.js
@@ -5,5 +5,11 @@
 var Collector = require('node-sflow');
 
 Collector(function(flow) {
+    if (flow && flow.flow.records && flow.flow.records.length>0) {
+        console.log(flow.flow);
 
+        flow.flow.records.forEach(function(n) {
+            console.log('process record',n);
+        });
+    }
 }).listen(6344);
